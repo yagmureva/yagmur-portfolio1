2b25990675ee6678ec7ef78125828478fcf8ba91
Yağmur Nielsen <yagmureva4@gmail.com>
2025-01-29 13:33:14 +0100

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Link } from "@material-ui/core";
 import { TextDecrypt } from "../content/TextDecrypt";
-import ResumePDF from "./../../assets/YagmurNielsenResume.pdf";
+//import ResumePDF from "./../../assets/YagmurNielsenResume.pdf";
 import { ResumeIcon } from "../content/ResumeButton";
 
 const useStyles = makeStyles((theme) => ({
